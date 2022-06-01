b1095b3d6ca66821004cbdd40ffc962a84eb52dc
luccaloopz <luccamartins2012@gmail.com>
2022-05-31 17:15:46 -0700

    added to the Portfolio component which takes the data from the array within projects.json and applies the built-in map array method to create bootstrap cards for each project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
-export default function Project() {
+export default function Project(props) {
     return (
         <Card>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={props.image}/>
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{props.title}</Card.Title>
                 <Card.Text>
-                Some quick example text to build on the card title and make up the bulk of
-                the card's content.
+                {props.text}
                 </Card.Text>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Card.Link href={props.githubLink} style={{color: 'black'}} target={'_blank'}>GitHub Repo</Card.Link>
+                <Card.Link href={props.deployedLink} style={{color: 'black'}} target={'_blank'}>Deployed App</Card.Link>
             </Card.Body>
         </Card>
     )
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import Project from '../Project';
+import projects from '../../projects.json'
 
 export default function Portfolio() {
     return (
         <Row xs={1} md={2} className="g-4" style={{padding: 70}}>
-            {Array.from({ length: 6 }).map((_, idx) => (
+            {projects.map((project) => (
                 <Col>
-                    <Project />
+                    <Project key={project.id} image={project.image} title={project.title} text={project.text} githubLink={project.githubLink} deployedLink={project.deployedLink}/>
                 </Col>
             ))}
         </Row>
-        
     );
 }
