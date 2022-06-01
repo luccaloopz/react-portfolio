a8e57971a15855becb77c2ef316236a94daf5b7d
luccaloopz <luccamartins2012@gmail.com>
2022-06-01 16:00:04 -0700

    when user clicks on any one of the contact form input fields and leaves it without entering anything, ta message displays at the bottom of the form.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,50 +2,36 @@ import React, { useState } from 'react';
 import { FloatingLabel, Form, Button, Card } from 'react-bootstrap';
 
 export default function Contact() {
-  // const [validated, setValidated] = useState(false);
   const [message, setMessage] = useState('');
 
-  // const handleSubmit = (event) => {
-  //   const form = event.currentTarget;
-  //   if (form.checkValidity() === false) {
-  //     event.preventDefault();
-  //     event.stopPropagation();
-  //   }
+  const handleChange = (event) => {
+    const inputField = event.target.id;
 
-  //   setValidated(true);
-  // };
-
-  const nameInput = {}
-
-  const handleChange = () => {
-    // setMessage(event.target.value);
-    if (nameInput.value.length === 0) {
-      setMessage('Name must be included')
-    }
+    if (event.target.value === '') {
+      setMessage(`${inputField} field must be included`)
+    };
     
   };
 
   return (
-    // <Form noValidate validated={validated} onSubmit={handleSubmit} >
       <Card hasValidation style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', marginLeft: '25%', marginRight: '25%', marginTop: '5%', padding: '10px'}} border="dark" bg='light'>
         <Card.Title className="mb-3">Contact Me</Card.Title>
-        <FloatingLabel controlId="floatingInput" label="Name" className="mb-3">
-          <Form.Control type="name" placeholder="John Doe" required/>
+        <FloatingLabel label="Name" className="mb-3">
+          <Form.Control id='Name' type="name" placeholder="John Doe" onBlur={handleChange} required />
         </FloatingLabel>
 
-        <FloatingLabel controlId="floatingInput" label="Email address"  className="mb-3">
-          <Form.Control type="email" placeholder="name@example.com" required />
+        <FloatingLabel label="Email address"  className="mb-3">
+          <Form.Control id='Email' type="email" placeholder="name@example.com" onBlur={handleChange} required />
         </FloatingLabel>
 
-        <FloatingLabel controlId="floatingTextarea" label="Message" className="mb-3">
-          <Form.Control as="textarea" placeholder="Leave your message here" required />
+        <FloatingLabel label="Message" className="mb-3">
+          <Form.Control id='Message' as="textarea" placeholder="Leave your message here" onBlur={handleChange} required />
         </FloatingLabel>
 
         <Form.Group>
             <Button type="submit">Submit</Button>
         </Form.Group>
-        <p style={{paddingTop: 30}} onChange={handleChange}>{message}</p>
+        <p style={{paddingTop: 15}}>{message}</p>
       </Card>
-    // </Form>
   )
 }
