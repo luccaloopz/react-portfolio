4dbb89e82fa26d0786df3f261647c39d30200ce0
luccaloopz <luccamartins2012@gmail.com>
2022-06-14 12:35:41 -0700

    added functionality to the contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -3,13 +3,28 @@ import { FloatingLabel, Form, Button, Card } from 'react-bootstrap';
 
 export default function Contact() {
   const [message, setMessage] = useState('');
+  const [email, setEmail] = useState({
+    name: '',
+    email: '',
+    body: '',
+  });
 
   const emailValidation = (email) => {
     return String(email).toLowerCase().match(/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/);
   };
 
+  const sendEmail = () => {
+    console.log(email.body);
+
+    return window.location = `mailto:luccamartins2011@yahoo.com?&subject=*Enter The Subject Line Here*
+&body=Hello Lucca, My name is ${email.name}. ${email.body}`
+  };
+
   const handleChange = (event) => {
+    event.preventDefault();
+
     const inputField = event.target.id;
+    const value = event.target.value;
 
     if (event.target.id === 'Email') {
       const emailValue = event.target.value;
@@ -23,6 +38,11 @@ export default function Contact() {
     if (event.target.value === '') {
       setMessage(`${inputField} field must be included`)
     };
+
+    setEmail({
+      ...email,
+      [event.target.name]: value,
+    });
     
   };
 
@@ -30,19 +50,19 @@ export default function Contact() {
       <Card hasValidation style={{display: 'flex', justifyContent: 'center', flexDirection: 'column', marginLeft: '25%', marginRight: '25%', marginTop: '5%', padding: '10px', backgroundColor: '#C2DED1'}} border="dark">
         <Card.Title className="mb-3">Contact Me</Card.Title>
         <FloatingLabel label="Name" className="mb-3">
-          <Form.Control id='Name' type="name" placeholder="John Doe" onBlur={handleChange} required />
+          <Form.Control id='Name' type="name" name='name' placeholder="John Doe" value={email.name} onChange={handleChange} required />
         </FloatingLabel>
 
         <FloatingLabel label="Email address"  className="mb-3">
-          <Form.Control id='Email' type="email" placeholder="name@example.com" onBlur={handleChange} required />
+          <Form.Control id='Email' type="email" name='email' placeholder="name@example.com" value={email.email} onChange={handleChange} required />
         </FloatingLabel>
 
-        <FloatingLabel label="Message" className="mb-3">
-          <Form.Control id='Message' as="textarea" placeholder="Leave your message here" onBlur={handleChange} required />
+        <FloatingLabel label="Body" className="mb-3">
+          <Form.Control id='Body' as="textarea" name='body' placeholder="Leave your message here" value={email.body} onChange={handleChange} required />
         </FloatingLabel>
 
         <Form.Group>
-            <Button type="submit">Submit</Button>
+            <Button type="submit" onClick={sendEmail}>Submit</Button>
         </Form.Group>
         <p style={{paddingTop: 15}}>{message}</p>
       </Card>
